69fba3a5bb8f093f80201aa976278ef7c3264742
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-20 17:45:29 +0800

    調整 package

--- TogetherServer/src/main/java/com/exfantasy/server/controller/GreetingController.java
+++ TogetherServer/src/main/java/com/exfantasy/server/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.exfantasy.server.web;
+package com.exfantasy.server.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- TogetherServer/src/main/java/com/exfantasy/server/controller/HomeController.java
+++ TogetherServer/src/main/java/com/exfantasy/server/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.exfantasy.server.web;
+package com.exfantasy.server.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
