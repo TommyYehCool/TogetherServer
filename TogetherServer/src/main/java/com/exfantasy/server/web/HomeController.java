96612c5baad6f6433cd083761eac6a804d037492
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-20 14:16:05 +0800

    1. modify .gitignore 2. update HomeContorller

--- TogetherServer/src/main/java/com/exfantasy/server/web/HomeController.java
+++ TogetherServer/src/main/java/com/exfantasy/server/web/HomeController.java
@@ -2,12 +2,9 @@ package com.exfantasy.server.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.exfantasy.server.vo.Message;
@@ -16,11 +13,8 @@ import com.exfantasy.server.vo.Message;
 @RequestMapping("/*")
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger("com.exfantasy.server");
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-//	@Autowired
-//    protected JdbcTemplate jdbc;
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
