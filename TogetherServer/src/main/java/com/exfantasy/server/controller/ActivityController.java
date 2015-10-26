8cc3843d9e38968b2583ecb2362907faf332f811
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-26 15:09:45 +0800

    調整活動 controller

---
+++ TogetherServer/src/main/java/com/exfantasy/server/controller/ActivityController.java
@@ -0,0 +1,20 @@
+package com.exfantasy.server.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/activity")
+public class ActivityController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ActivityController.class);
+
+	@RequestMapping(value = "/activity", method = RequestMethod.POST, produces = "application/json")
+	public String create() {
+		return "Return from ActivityController create";
+	}
+
+}
