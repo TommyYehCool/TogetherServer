4dc8ef25fdde5361f1793070ef8d2f705bada7be
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-27 13:05:47 +0800

    新增發送 email 機制

--- TogetherServer/src/main/java/com/exfantasy/server/controller/MailController.java
+++ TogetherServer/src/main/java/com/exfantasy/server/controller/MailController.java
@@ -1,25 +1,33 @@
 package com.exfantasy.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/sendmail")
+@RequestMapping(value = "/mail")
 public class MailController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MailController.class);
 	
 	@Autowired
 	private JavaMailSender mailSender;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/send", method = RequestMethod.GET)
+	@ResponseBody
 	public String sendMail() {
+		logger.info(">>>>>>> In MailController");
+		
 		SimpleMailMessage mail = new SimpleMailMessage();
-		mail.setTo("tommy.yeh1112@gmail.com");
-		mail.setSubject("Test from SpringBoot");
-		mail.setText("This is a test from SpringBoot");
+		mail.setTo("bensn11@gmail.com");
+		mail.setSubject("This is a test from SpringBoot");
+		mail.setText("Fuck you benson");
 		mailSender.send(mail);
 		return "Send mail succeed";
 	}
