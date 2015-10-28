ea8a0ba9710d3e3c5bc73894821c657562b65e9c
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-28 14:59:01 +0800

    先把 security mark 掉方便測試, 等研究出來怎麼做在說

--- TogetherServer/src/main/java/com/exfantasy/server/config/SecurityConfig.java
+++ TogetherServer/src/main/java/com/exfantasy/server/config/SecurityConfig.java
@@ -17,24 +17,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest()		// 對象為所有網址  
-			.authenticated()	// 存取必須通過驗證  
-			.and()
-			.formLogin()		// 若未不符合authorize條件，則產生預設login表單  
-			.and()
-			.httpBasic();		// 產生基本表單  
+//		http.authorizeRequests()
+//			.anyRequest()		// 對象為所有網址  
+//			.authenticated()	// 存取必須通過驗證  
+//			.and()
+//			.formLogin()		// 若未不符合authorize條件，則產生預設login表單  
+//			.and()
+//			.httpBasic();		// 產生基本表單  
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication() // 自訂Runtime時的使用者帳號  
-            .withUser("admin") 		  // 新增user  
-            .password("ilovebmw")     // 指定密碼  
-            .roles("ADMIN", "USER")   // 指派權限群組  
-            .and() 					  // 再新增使用者  
-            .withUser("user")  
-            .password("user12345")  
-            .roles("USER");  
+//		auth.inMemoryAuthentication() // 自訂Runtime時的使用者帳號  
+//            .withUser("admin") 		  // 新增user  
+//            .password("ilovebmw")     // 指定密碼  
+//            .roles("ADMIN", "USER")   // 指派權限群組  
+//            .and() 					  // 再新增使用者  
+//            .withUser("user")  
+//            .password("user12345")  
+//            .roles("USER");  
 	}
 }
