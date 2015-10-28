f94c906da68b2bfffdec17aecdc78ad65a80fb7d
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-28 11:09:57 +0800

    新增 resource config

---
+++ TogetherServer/src/main/java/com/exfantasy/server/config/ResourceConfig.java
@@ -0,0 +1,21 @@
+package com.exfantasy.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class ResourceConfig extends WebMvcConfigurerAdapter {
+	
+	@Value("${store.file.path}")
+	private String STORE_FILE_PATH;
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!STORE_FILE_PATH.endsWith("/")) {
+			STORE_FILE_PATH += "/";
+		}
+		registry.addResourceHandler("/images/**").addResourceLocations("file://" + STORE_FILE_PATH);
+	}
+}
