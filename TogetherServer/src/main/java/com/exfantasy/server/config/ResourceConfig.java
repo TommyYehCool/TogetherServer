33f09317065d1fdcbf426af8e3575ebdf21aefcf
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-28 11:47:39 +0800

    import 最佳化

--- TogetherServer/src/main/java/com/exfantasy/server/config/ResourceConfig.java
+++ TogetherServer/src/main/java/com/exfantasy/server/config/ResourceConfig.java
@@ -2,7 +2,6 @@ package com.exfantasy.server.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
