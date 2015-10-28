19c5310fedf36e99fc558e157efaf9c96ce1f02c
TommyYehCool <tommy.yeh1112@gmail.com>
2015-10-28 11:47:06 +0800

    1.調整 ResourceConifg annotation, 2.把 mail.properties 拿掉,移回 application.properties

--- TogetherServer/src/main/java/com/exfantasy/server/config/ResourceConfig.java
+++ TogetherServer/src/main/java/com/exfantasy/server/config/ResourceConfig.java
@@ -1,11 +1,12 @@
 package com.exfantasy.server.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Component
+@Configuration
 public class ResourceConfig extends WebMvcConfigurerAdapter {
 	
 	@Value("${store.file.path}")
